45785134f9c590c2542d1500afb2f7b0b814115d
francojcuevas <54785168+francojcuevas@users.noreply.github.com>
2022-03-28 10:17:21 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,4 +3,4 @@
 projectFile=$1
 csvFIle=$2
 reportFIle=$3
-docker run --rm -v $WORKSPACE/jmeterPrueba:/workspace swethapn14/repo_perf:JmeterLatest -Jjmeter.save.saveservice.output_format=xml -Jcsvfile=/workspace/csvFile -n -t /workspace/$projectFile -l /workspace/$reporteFile
+sudo docker run --rm -v $WORKSPACE/jmeterPrueba:/workspace swethapn14/repo_perf:JmeterLatest -Jjmeter.save.saveservice.output_format=xml -Jcsvfile=/workspace/csvFile -n -t /workspace/$projectFile -l /workspace/$reporteFile
