636a7ad31b2d8ed030f4d95a5b7149662dbe616a
francojcuevas <54785168+francojcuevas@users.noreply.github.com>
2022-03-31 17:37:22 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@
 #reportFile=$3
 cat cuenta.csv
 docker run --rm -v $WORKSPACE:/workspace swethapn14/repo_perf:JmeterLatest -Jjmeterengine.stopfail.system.exit=true -Jjmeter.save.saveservice.output_format=xml -Jcsvfile=/workspace/cuenta.csv -n -t /workspace/falla.jmx -l /workspace/reportejenkins.jtl
-if [grep "false" $WORKSPACE/reportejenkins.jtl] 
+if grep "false" $WORKSPACE/reportejenkins.jtl 
 then 
 echo El test fallo
 exit 1
