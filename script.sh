beb0424e4de2384b1cef99075cbee5b3f61ed462
francojcuevas <54785168+francojcuevas@users.noreply.github.com>
2022-04-05 12:07:22 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,9 +2,8 @@
 csvFile=$1
 projectName=$2
 reportFile=$3
-CURRENTDATE=`date +"%Y-%m-%d %T"`
-echo /////////////////7
-echo Current Date is: ${CURRENTDATEONLY}
+echo //////////////////////////////
+date
 docker run --rm -v $WORKSPACE:/workspace swethapn14/repo_perf:JmeterLatest -Jjmeterengine.stopfail.system.exit=true -Jjmeter.save.saveservice.output_format=xml -Jcsvfile=/workspace/$csvFile -n -t /workspace/$projectName -l /workspace/$reportFile
 #if grep "false" $WORKSPACE/reportejenkins.jtl
 #then 
@@ -16,7 +15,8 @@ if grep "false" $reportFile > resultadoemail.txt && $JOB_NAME > resultadoemail.t
 then
 echo El test fallo
 exit 1
-else grep "true" $reportFile > resultadoemail.txt && grep $JOB_NAME > resultadoemail.txt && grep $DATE > resultadoemail.txt
+else grep "true" $reportFile > resultadoemail.txt 
+#&& grep $JOB_NAME > resultadoemail.txt && grep $DATE > resultadoemail.txt
 echo El test finalizo correctamente
 exit 0
 fi
