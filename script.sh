0b7dbec180801b67f3352e73ec54b0664fa18be1
francojcuevas <54785168+francojcuevas@users.noreply.github.com>
2022-04-05 11:47:15 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,7 +2,7 @@
 csvFile=$1
 projectName=$2
 reportFile=$3
-echo %DATE%
+echo date
 docker run --rm -v $WORKSPACE:/workspace swethapn14/repo_perf:JmeterLatest -Jjmeterengine.stopfail.system.exit=true -Jjmeter.save.saveservice.output_format=xml -Jcsvfile=/workspace/$csvFile -n -t /workspace/$projectName -l /workspace/$reportFile
 #if grep "false" $WORKSPACE/reportejenkins.jtl
 #then 
