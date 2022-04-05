0e61fa759a4088c1056b6642ec57e3d2511d00f5
francojcuevas <54785168+francojcuevas@users.noreply.github.com>
2022-04-05 16:10:31 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ reportFile=$3
 var_date=$(date +%c)
 rm $reporFile
 docker run --rm -v $WORKSPACE:/workspace swethapn14/repo_perf:JmeterLatest -Jjmeterengine.stopfail.system.exit=true -Jjmeter.save.saveservice.output_format=xml -Jcsvfile=/workspace/$csvFile -n -t /workspace/$projectName -l /workspace/$reportFile
-if grep "false" $reportFile > resultadoemail.txt && $JOB_NAME > resultadoemail.txt && $DATE > resultadoemail.txt
+if grep "false" $reportFile > resultadoemail.txt && echo "Nombre de proyecto:" $JOB_NAME >> resultadoemail.txt && echo "Fecha y hora de ejecucion:" $var_date >> resultadoemail.txt && echo "El test de Performance fallo"
 then
 echo El test fallo
 exit 1
