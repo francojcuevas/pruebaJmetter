d06ecd9f5e3bd2001905a4dfea10d2be2d5d9f02
francojcuevas <54785168+francojcuevas@users.noreply.github.com>
2022-04-05 15:22:50 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,19 +2,15 @@
 csvFile=$1
 projectName=$2
 reportFile=$3
-date
 #date + "%FORMAT"
-var=$(date)
-echo //////////////////////////////
-echo "$var"
-echo //////////////////////////////
-docker run --rm -v $WORKSPACE:/workspace swethapn14/repo_perf:JmeterLatest -Jjmeterengine.stopfail.system.exit=true -Jjmeter.save.saveservice.output_format=xml -Jcsvfile=/workspace/$csvFile -n -t /workspace/$projectName -l /workspace/$reportFile
+var_date=$(date)
 
+docker run --rm -v $WORKSPACE:/workspace swethapn14/repo_perf:JmeterLatest -Jjmeterengine.stopfail.system.exit=true -Jjmeter.save.saveservice.output_format=xml -Jcsvfile=/workspace/$csvFile -n -t /workspace/$projectName -l /workspace/$reportFile
 if grep "false" $reportFile > resultadoemail.txt && $JOB_NAME > resultadoemail.txt && $DATE > resultadoemail.txt
 then
 echo El test fallo
 exit 1
-else echo "\n$JOB_NAME" > resultadoemail.txt && echo "date" >> resultadoemail.txt && grep "true" $reportFile >> resultadoemail.txt
+else echo "\n$JOB_NAME" > resultadoemail.txt && echo "$var_date" >> resultadoemail.txt && grep "true" $reportFile >> resultadoemail.txt
 #&& echo "\n$JOB_NAME" > resultadoemail.txt
 #&& grep $JOB_NAME > resultadoemail.txt && grep $DATE > resultadoemail.tx
 echo El test finalizo correctamente
